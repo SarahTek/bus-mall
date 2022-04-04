2e836e5663a75f046a0206774a2fd66617a55452
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-04-04 17:41:52 -0400

    updated index.html,style.css, and app.js

--- js/app.js
+++ js/app.js
@@ -5,60 +5,82 @@ let image1 = document.getElementById('image1');
 let image2 = document.getElementById('image2');
 let image3 = document.getElementById('image3');
 
-// getting the ul from the DOM to manipulate
+
 let results = document.getElementById('display-results');
 const imageArray = [];
 let maxClick = 25;
 let totalClicks = 0;
 
-//constructor function
+
 function ListedImages(name, filePath = 'jpg') {
   this.name = name;
   this.src = `img/${name}.${filePath}`;
-  this.timesShown = 0;
-  this.timesClicked = 0;
   this.votes = 0;
   this.views = 0;
-  imageArray.push(this);
 
 }
-//instantiate
-new ListedImages('bag');
-new ListedImages('bathroom');
-new ListedImages('boots');
-new ListedImages('breakfast');
-new ListedImages('bubblegum');
-new ListedImages('chair');
-new ListedImages('cthulhu');
-new ListedImages('dog-duck');
-new ListedImages('dragon');
-new ListedImages('pen');
-new ListedImages('pet-sweep');
-new ListedImages('scissors');
-new ListedImages('shark');
-new ListedImages('sweep', 'png');
-new ListedImages('tauntaun');
-new ListedImages('unicorn');
-new ListedImages('water-can');
-new ListedImages('wine-glass');
 
-console.log(imageArray);
+
+function showProducts() {
+  imageArray.push(new ListedImages('bag'));
+  imageArray.push(new ListedImages('bathroom'));
+  imageArray.push(new ListedImages('boots'));
+  imageArray.push(new ListedImages('breakfast'));
+  imageArray.push(new ListedImages('bubblegum'));
+  imageArray.push(new ListedImages('chair'));
+  imageArray.push(new ListedImages('cthulhu'));
+  imageArray.push(new ListedImages('dog-duck'));
+  imageArray.push(new ListedImages('dragon'));
+  imageArray.push(new ListedImages('pen'));
+  imageArray.push(new ListedImages('pet-sweep'));
+  imageArray.push(new ListedImages('scissors'));
+  imageArray.push(new ListedImages('shark'));
+  imageArray.push(new ListedImages('sweep', 'png'));
+  imageArray.push(new ListedImages('tauntaun'));
+  imageArray.push(new ListedImages('unicorn'));
+  imageArray.push(new ListedImages('water-can'));
+  imageArray.push(new ListedImages('wine-glass'));
+}
+
+itemStorage();
+function itemStorage() {
+  if (localStorage.getItem('imageArray')) {
+    let parseImages = JSON.parse(localStorage.getItem('imageArray'));
+    for (let i = 0; i < parseImages.length; i++) {
+      console.log(parseImages[i]);
+      let newProduct = new ListedImages(parseImages[i].name, (parseImages[i].src));
+      newProduct.src = parseImages[i].src;
+      newProduct.votes = parseImages[i].votes;
+      newProduct.views = parseImages[i].views;
+      imageArray.push(newProduct);
+    }
+  } else {
+    showProducts();
+  }
+}
+function saveProducts() {
+  let imageStringified = JSON.stringify(imageArray);
+  localStorage.setItem('imageArray', imageStringified);
+
+}
+
 
 function randomImage() {
   let randomIndex = Math.floor(Math.random() * imageArray.length);
   return randomIndex;
 }
 
+let uniqueProductIndexes = [];
 function renderImages() {
-  let uniqueProductIndexes = [];
 
   while (uniqueProductIndexes.length < 6) {
     let num = randomImage();
-    if (!uniqueProductIndexes.includes(num)) {
-      uniqueProductIndexes.push(num);
+    while (uniqueProductIndexes.includes(num)) {
+      num = randomImage();
     }
+    uniqueProductIndexes.push(num);
   }
-  console.log(uniqueProductIndexes);
+
   let index1 = uniqueProductIndexes.shift();
   let index2 = uniqueProductIndexes.shift();
   let index3 = uniqueProductIndexes.shift();
@@ -92,16 +114,13 @@ function handleImageClick(event) {
   }
   if (totalClicks === maxClick) {
     imageContainer.removeEventListener('click', handleImageClick);
+    saveProducts();
   }
   renderImages();
 }
 
 
-
-
 function handleResults() {
-  // maxClick++;
-  // let maxClick = event.target.alt;
   if (totalClicks === maxClick) {
     for (let i = 0; i < imageArray.length; i++) {
       let li = document.createElement('li');
@@ -110,88 +129,59 @@ function handleResults() {
     }
     showResults.removeEventListener('click', handleResults);
   }
+  renderChart();
 }
-// renderImages();
-
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Bag', 'Banana', 'Bathroom', 'Boots', 'Breakfast', 'Bubblegum'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgba(255, 99, 132, 0.4)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1,
-      // hoverBorderColor: 'rgba(250, 50, 122, 1)',
-    },
-    {
-      label: '# of Views',
-      data: [17, 15, 5, 2, 4, 6],
-      backgroundColor: 'rgba(153, 102, 255, 0.4)',
-      borderColor: 'rgba(153, 102, 255, 1)',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    },
-
-    //it fits to the box or screen that's put inside of / its a default
-    responsive: true
-  }
-});
-
 
 imageContainer.addEventListener('click', handleImageClick);
 showResults.addEventListener('click', handleResults);
 
 
+function renderChart() {
+  let views = [];
+  let votes = [];
+  let name = [];
+
+  for (let i = 0; i < imageArray.length; i++) {
+    let ListedImages = imageArray[i];
+    views.push(ListedImages.views); 
+    votes.push(ListedImages.votes);
+    name.push(ListedImages.name);
+  }
+  console.log(views);
 
 
 
+  const ctx = document.getElementById('myChart').getContext('2d');
 
+  let config = {
+    type: 'bar',
+    data: {
+      labels: name,
+      datasets: [{
+        label: '# of Votes',
+        data: votes,
+        backgroundColor: 'rgba(163, 0, 60, 0.4)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+      {
+        label: '# of Views',
+        data: views,
+        backgroundColor: 'rgba(153, 102, 255)',
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
 
+      responsive: true
+    }
+  };
+  const myChart = new Chart(ctx, config);
 
-
-
-
-
-
-
-
-
-
-
-
-// function getThreeImages() {
-//    let getThreeImages = '';
-
-// while (getThreeImages.length < 3) {
-//   let num = randomImage();
-//   while (getThreeImages.includes(num)) {
-//     num = randomImage();
-//   }
-//   getThreeImages.push(num);
-// }
-// console.log(getThreeImages);
-
-
-// let leftImage = imageArray[randomImage()];
-// let rightImage = imageArray[randomImage()];
-// let middleImage = imageArray[randomImage()];
-// image1.src = leftImage.filePath;
-// image3.src = rightImage.filePath;
-// image2.src = middleImage.filePath;
-// image1.alt = leftImage.name;
-// image3.alt = rightImage.name;
-// image2.alt = middleImage.name;
-// leftImage.timesShown++;
-// rightImage.timesShown++;
-// middleImage.timesShown++;
-
-// }
+}
